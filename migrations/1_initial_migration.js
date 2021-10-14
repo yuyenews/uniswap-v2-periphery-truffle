c0b70863acab7454a89aad057353c4cf8d4b7403
yuyenews <yuyemail@163.com>
2021-10-14 13:14:22 +0800

    init commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,7 @@
-const Migrations = artifacts.require("Migrations");
 const UniswapV2Router01 = artifacts.require("UniswapV2Router01");
 const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
   deployer.deploy(UniswapV2Router01, "factory address", "WETH9");
   deployer.deploy(UniswapV2Router02, "factory address", "WETH9");
 };
